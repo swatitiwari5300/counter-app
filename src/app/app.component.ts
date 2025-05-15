f8261ee1fb3fa5004bfe45c8c5f16de4b60b669d
swatitiwari5300 <swatitiwari5300@gmail.com>
2025-05-16 00:40:02 +0530

    Counter app changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,29 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'counter-app';
+  count:number = 0;
+
+  handleIncrement(){
+    this.count = this.count+1;
+  }
+
+    handleDecrement(){
+    this.count = this.count-1;
+  }
+
+  handleReset(){
+    this.count = 0;
+  }
+
+  handleCounter(val:string){
+    if(val === '-'){
+      if(this.count>0){
+      this.handleDecrement();
+      }
+    }else if(val === '+'){
+      this.handleIncrement();
+    }else{
+      this.handleReset();
+    }
+  }
 }
